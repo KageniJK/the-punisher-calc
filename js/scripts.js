8856dc77a518cbf2c404fb2e735135ba46befa9c
James Kageni <james.kageni@gmail.com>
2018-07-12 12:41:24 +0300

    commented on the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
   $(".form1 form").submit(function(event){
-    event.preventDefault();
-    var numberString = $("input#add1").val();
-    var numberSums = numberString.split(",");
-    numberSumms = numberSums.map( num1 => {
+    event.preventDefault();                   //preventing default
+    var numberString = $("input#add1").val(); //assigning the input to a variable
+    var numberSums = numberString.split(","); //splitting the string into an array
+    numberSumms = numberSums.map( num1 => {   //turning an array of strings into 1 of integers
       return parseInt(num1);
     })
     var sum = 0;
-    var add = numberSumms.map( num =>{
+    var add = numberSumms.map( num =>{        //adding up the numbers in the array
       return sum += num;
     });
 
-    console.log(sum);
+    console.log(sum);                         //display the total in the console
+
+    $("#output").text(sum);                   //display the output in the html 
 
 
   });
