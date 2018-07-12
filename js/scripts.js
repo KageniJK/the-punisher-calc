47ec84203aafedeb20bd381d242e55e25fc514cc
James Kageni <james.kageni@gmail.com>
2018-07-12 12:14:33 +0300

    completed addition function

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
   $(".form1 form").submit(function(event){
     event.preventDefault();
     var numberString = $("input#add1").val();
-    var numberArray = numberString.split(",");
-    console.log(numberArray);
+    var numberSums = numberString.split(",");
+    numberSumms = numberSums.map( num1 => {
+      return parseInt(num1);
+    })
+    var sum = 0;
+    var add = numberSumms.map( num =>{
+      return sum += num;
+    });
+
+    console.log(sum);
 
 
   });
